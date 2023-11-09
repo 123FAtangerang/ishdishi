8d87bd9edb342575395017fd6751f88f2111901a
123FAtangerang <149584464+123FAtangerang@users.noreply.github.com>
2023-11-09 09:04:10 +0700

    Add files via upload

---
+++ data/PipaEksisting_3.js
@@ -0,0 +1,8 @@
+var json_PipaEksisting_3 = {
+"type": "FeatureCollection",
+"name": "PipaEksisting_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Jenis": "HDPE" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 106.630238916031047, -6.19591636196725 ], [ 106.63023477633034, -6.196000278657829 ], [ 106.627229486621673, -6.195907028515033 ], [ 106.624790737170187, -6.195761558259381 ], [ 106.624396785335719, -6.195746638230889 ], [ 106.623803981622899, -6.195515377735367 ], [ 106.62352206474192, -6.195390982504333 ] ] ] } }
+]
+}
---
+++ data/Reservoir_5.js
@@ -0,0 +1,8 @@
+var json_Reservoir_5 = {
+"type": "FeatureCollection",
+"name": "Reservoir_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Smber Dana": "RKAP", "Kapasitas": "4000 M3", "Jns Bngnan": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 106.538025685899512, -6.137486693508924 ], [ 106.538092345438372, -6.136968072130885 ], [ 106.538815601435104, -6.137198386479287 ], [ 106.538773939223319, -6.137554627901236 ], [ 106.538025685899512, -6.137486693508924 ] ] ] ] } }
+]
+}
